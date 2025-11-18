5e05ccd03cc7552981ec0cd83e4a48322525314d
Lasse Thomsen <lasse.thomsen@hotmail.dk>
2025-11-18 16:05:27 +0100

    added secret page for testing of roles

---
+++ src/routes/secret/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let data;
+    const user = data.user;
+</script>
+
+{#if user?.role === "ADMIN"}
+    ADMIN ADMIN ADMIN
+{/if}
+{#if user?.role === "USER"}
+   user user user
+{/if}
+{user?.name}
