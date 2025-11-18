7e1105e6fad012ad6db98b853d778f44ae4506ab
Lasse Thomsen <lasse.thomsen@hotmail.dk>
2025-11-18 15:35:53 +0100

    hook now populates event.locals.user and session

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,16 @@
 import { auth } from "$lib/auth/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
-import { building } from '$app/environment'
+import { building } from "$app/environment";
 
-export async function handle({ event, resolve }) {
+export const handle = async ({ event, resolve }) => {
+    const response = await auth.api.getSession({
+        headers: event.request.headers,
+    });
+
+    if (response) {
+        event.locals.session = response.session;
+        event.locals.user = response.user;
+    }
+
     return svelteKitHandler({ event, resolve, auth, building });
-}
+};
