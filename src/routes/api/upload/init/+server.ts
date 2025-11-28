e43900d37dfd4722b91d047f4205c6c59c71bfa6
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-11-28 12:51:24 +0100

    fixed auth check

--- src/routes/api/upload/init/+server.ts
+++ src/routes/api/upload/init/+server.ts
@@ -4,21 +4,19 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { prisma } from '$lib/server/prisma';
 import { s3 } from '$lib/S3/S3-client';
 import { BUCKET_NAME } from '$env/static/private';
+import { auth } from '$lib/auth/auth';
 
 
 
-export async function POST({ request, locals }) {
-	const session = await locals.auth.api.getSession();
+export async function POST({ request }) {
+	const session = await auth.api.getSession({
+		headers: request.headers
+	});
 	if (!session?.user) {
 		return json({ error: 'Unauthorized' }, { status: 401 });
 	}
 
 	const user = session.user;
-
-	const { size, contentType } = await request.json();
-	if (!size || !contentType) {
-		return json({ error: 'Invalid body' }, { status: 400 });
-	}
 
 	const fileId = crypto.randomUUID();
 	const s3Key = `uploads/${fileId}.bin`;
