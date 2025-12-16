96cd065bae58dab48400fc199c70ed3b92e572f9
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-12-16 15:26:48 +0100

    tweaked ui

--- src/lib/Components/share/CreateShareLink.svelte
+++ src/lib/Components/share/CreateShareLink.svelte
@@ -12,10 +12,10 @@
 	}
 </script>
 
-<div class="p-3 border bg-black w-72 rounded">
-	<p class="text-sm mb-2 text-gray-300">Link expires in (minutes):</p>
+<div class="p-3 border bg-black w-72">
+	<p class="text-sm mb-2">Link expires in: </p>
 
-	<div class="flex items-center gap-2 p-2 border border-gray-700 rounded bg-gray-800">
+	<div class="flex items-center gap-2 p-2 border">
 		<input
 			type="number"
 			min="1"
@@ -23,11 +23,11 @@
 			placeholder="Expiration in minutes…"
 			bind:value={expireInMinutes}
 		/>
-		<span class="text-gray-400 text-sm">min</span>
+		<span class="text-sm">min</span>
 	</div>
 
 	<button
-		class="mt-3 w-full border px-3 py-2 hover:bg-gray-700 rounded"
+		class="mt-3 w-full border px-3 py-2 hover:bg-gray-700"
 		on:click={submit}
 	>
 		Create Share Link
