efc7e578e8c823549b0ffa52a5d1c895774bcb3c
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-12-16 17:54:17 +0100

    refactored to use mnemonic to seed keypair

--- src/lib/auth/signUp.ts
+++ src/lib/auth/signUp.ts
@@ -1,40 +1,36 @@
-import { authClient } from "$lib/auth/auth-client";
-import { generateMnemonic } from "@scure/bip39";
-import { wordlist } from '@scure/bip39/wordlists/english.js';
-import { set as idbSet } from "idb-keyval";
+import { authClient } from '$lib/auth/auth-client';
 import sodium from 'libsodium-wrappers';
+import { generateMnemonic } from '@scure/bip39';
+import { wordlist } from '@scure/bip39/wordlists/english.js';
+import { set as idbSet } from 'idb-keyval';
 
 export async function signUp(name: string, email: string, password: string) {
 	await sodium.ready;
 
-	// X25519
-	const keypair = sodium.crypto_box_keypair();
+	const mnemonic = generateMnemonic(wordlist, 128);
+
+	const seed = sodium.crypto_generichash(32, new TextEncoder().encode(mnemonic));
+
+	const keypair = sodium.crypto_box_seed_keypair(seed);
 
 	const publicKeyBase64 = sodium.to_base64(keypair.publicKey);
 	const privateKeyBase64 = sodium.to_base64(keypair.privateKey);
-
-	// mnemonic backup
-	const mnemonic = generateMnemonic(wordlist, 128);
-
 
 	const { data, error } = await authClient.signUp.email({
 		email,
 		password,
 		name,
-		publicKey: publicKeyBase64,
-
+		publicKey: publicKeyBase64
 	});
 
 	if (error) {
 		throw new Error(error.message);
 	}
 
-
 	await idbSet(`${name}_privateKey`, privateKeyBase64);
-	await idbSet(`${name}_mnemonic`, mnemonic);
 
 	return {
 		user: data.user,
-		mnemonic  // todo: vis det
+		mnemonic
 	};
 }
