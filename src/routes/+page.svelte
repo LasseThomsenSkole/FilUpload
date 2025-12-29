1d2e11b073e71875f7d0f061e79e946a52bd59e6
LasseThomsen <lasse.thomsen@hotmail.dk>
2025-12-29 14:58:33 +0100

    removed logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script>
-    import logo from '$lib/assets/logo.png';
 
 </script>
 <div class="flex justify-between">
     <a class="ml-2 hover:text-red-500" href="/about">About</a>
     <a class="mr-2 hover:text-red-500" href="/login">Login</a>
 </div>
-<img src={logo} alt="Logo" class="max-h-1/2 max-w-1/2 mx-auto mt-auto hover:invert"/>
