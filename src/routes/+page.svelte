479cc1cc70b9b6147b1c9d1df13e70e4a0b64507
LasseThomsen <lasse.thomsen@hotmail.dk>
2026-01-02 18:09:02 +0100

    redesigned landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,74 @@
-<script>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { authClient } from '$lib/auth/auth-client.ts';
 
+	export let data;
+	const user = data.user;
 </script>
-<div class="flex justify-between">
-    <a class="ml-2 hover:text-red-500" href="/about">About</a>
-    <a class="mr-2 hover:text-red-500" href="/login">Login</a>
+
+<div class="min-h-screen flex items-center justify-center px-6">
+	<div class="max-w-xl w-full text-center">
+
+		<h1 class="text-3xl font-bold mb-4">
+			Secure file sharing.
+			<span class="block text-gray-400">
+				End-to-end encrypted.
+			</span>
+		</h1>
+
+		<p class="text-gray-400 mb-8">
+			Upload, share, and manage confidential files.
+			Only you and your recipients can read them not even the server.
+		</p>
+
+		{#if user}
+			<div class="flex flex-col sm:flex-row gap-3 justify-center">
+
+				<button
+					class="border px-5 py-2 hover:bg-gray-800"
+					on:click={() => goto('/download')}
+				>
+					Go to your files
+				</button>
+
+				<button
+					class="border px-5 py-2 hover:bg-gray-800"
+					on:click={() => goto('/upload')}
+				>
+					Upload a file
+				</button>
+			</div>
+			<button
+				class="mt-4 text-sm text-gray-400 hover:text-white"
+				on:click={async () => await authClient.signOut({
+					fetchOptions: {
+						onSuccess: () => {
+							window.location.reload();
+						},
+					},
+				})}
+			>
+				Log out
+			</button>
+		{:else}
+			<div class="flex flex-col sm:flex-row gap-3 justify-center">
+
+				<button
+					class="border px-5 py-2 hover:bg-gray-800"
+					on:click={() => goto('/register')}
+				>
+					Get started
+				</button>
+
+				<button
+					class="border px-5 py-2 hover:bg-gray-800"
+					on:click={() => goto('/login')}
+				>
+					Log in
+				</button>
+
+			</div>
+		{/if}
+
+	</div>
 </div>
