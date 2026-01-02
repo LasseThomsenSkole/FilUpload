d9818000c481b844cea94188607c1250eab4503e
LasseThomsen <lasse.thomsen@hotmail.dk>
2026-01-02 16:43:33 +0100

    added mx-auto to center component and added spacing on button

--- src/lib/Components/login/WordPhraseDialog.svelte
+++ src/lib/Components/login/WordPhraseDialog.svelte
@@ -17,7 +17,7 @@
 {#if mnemonic === null || mnemonic === undefined}
 	<p>Loading...</p>
 {:else}
-	<div class="flex flex-col border w-64 h-64 p-4">
+	<div class="flex flex-col border w-64 h-64 p-4 mx-auto">
 		<div class="flex justify-between items-center">
 			<h1 class="text-center">Your word phrase</h1>
 			<button class="hover:text-red-600" on:click={copyToClipboard}>
@@ -27,11 +27,11 @@
 		<p class="border mt-4 p-2">
 			{mnemonic}
 		</p>
-		<p class="mt-4 text-sm text-gray-300">
+		<p class="mt-2 text-sm text-gray-300">
 			Copy the phrase and keep it safe. You will need it to login on other devices.
 		</p>
-		<button on:click={ async ()=> await goto("/upload")} class="mt-4 text-sm text-gray-300">
-			i have safely stored my word phrase
+		<button on:click={ async ()=> await goto("/upload")} class="mt-auto text-sm text-white hover:text-gray-300 underline">
+			I have safely stored my word phrase
 		</button>
 	</div>
 {/if}
