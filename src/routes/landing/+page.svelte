d662b8d7d094f7da5b357974defd1040e28bcb12
LasseThomsen <lasse.thomsen@hotmail.dk>
2026-01-09 00:55:20 +0100

    removed test routes

--- src/routes/landing/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import WordPhraseDialog from '$lib/Components/login/WordPhraseDialog.svelte';
-</script>
-
-<div class="mx-auto mt-10 max-w-md">
-	<WordPhraseDialog
-		mnemonic="host slab much impulse sentence electric good claim rail female thing feed"
-	/>
-</div>
